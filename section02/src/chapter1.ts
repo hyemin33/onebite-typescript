ae7e94c6ba0ebc9b4c663b56cb3883fa3c6b93cf
hyemin33 <joliie@naver.com>
2025-07-01 17:34:24 +0900

    원시타입과 리터럴타입

---
+++ section02/src/chapter1.ts
@@ -0,0 +1,18 @@
+let num1: number = 123;
+let numb2: number = NaN;
+let numb3: number = Infinity;
+
+let str1: string = "이건 쉽지";
+let str2: string = `가능요 ${num1}`;
+
+let bool1: boolean = false;
+let bool2: boolean = true;
+
+let null1: null = null;
+let unde1: undefined = undefined;
+
+// 리터럴 타입
+// 타입을 값 자체로 설정하면 그 값 외에는 다른 값을 넣을 수 없음.
+let numA: 10 = 10;
+let boolA: true = true;
+let strA: "이건 쉽지요" = "이건 쉽지요";
