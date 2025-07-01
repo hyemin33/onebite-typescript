896198182718147beee21798cdba1c0524f57e0c
hyemin33 <joliie@naver.com>
2025-07-01 19:59:00 +0900

    타입 좁히기

--- section03/src/chapter7.ts
+++ section03/src/chapter7.ts
@@ -2,10 +2,19 @@
  * 조건문 등을 이용해 넓은 타입에서 좁은 타입으로 타입을 상황에 따라 좁히는 방법
  */
 
-function func(value: number | string) {
+type Person = {
+    name: string;
+    age: number;
+}
+
+function func(value: number | string | Date | null | Person) {
     if(typeof value === 'number'){
         console.log(value.toFixed());
-    } else if(typeof value === 'string'){
+    } else if(typeof value === 'string'){ 
         console.log(value.toUpperCase());
+    } else if(value instanceof Date){
+        console.log(value.getTime());
+    } else if(value && "age" in value){ // age는 Person에만 있으며, value가 null이 아니어야하기 때문에 value &&를 추가한다.
+        console.log(`${value.name}은 ${value.age}살 입니다`)
     }
 }
