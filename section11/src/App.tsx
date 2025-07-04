0c17f5e8ea16595a0093ba7296065b696480f078
hyemin33 <joliie@naver.com>
2025-07-04 18:01:18 +0900

    todo input 입력 추가

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,8 +1,25 @@
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
+import Editor from "./components/Editor";
+
+interface Todo {
+  id: number;
+  content: string;
+}
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+    setTodos([...todos, { id: idRef.current++, content: text }]);
+  };
+
   return (
     <div className="App">
+      <h1>Todo</h1>
+      <Editor onClickAdd={onClickAdd} />
     </div>
   );
 }
